d1e38d6d39a6084442d294d51f8c623397d3fcd6
Collin <candrew022@gmail.com>
2016-05-17 02:05:11 -0400

    Add AccountKitLogin to routes

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import Workspace from './modules/Workspace';
 import Home from './modules/Home';
 import PusherApp from './modules/PusherApp';
+import AccountKitLogin from './modules/AccountKitLogin';
 
 export default (
   <Route path="/" component={Workspace}>
     <IndexRoute component={Home}/>
     <Route path="pusher" component={PusherApp}/>
+    <Route path="accountkit" component={AccountKitLogin}/>
   </Route>
 )
